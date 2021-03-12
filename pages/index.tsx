c533d532ffc1c598f3cb407df77fc7e6266403ce
Peder Smith <smith.peder@gmail.com>
2021-03-12 14:09:43 +0100

    Clean some stuff

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,9 @@
 import useSWR from "swr";
-import socketIOClient, { Socket } from "socket.io-client";
+import socketIOClient from "socket.io-client";
 import styles from "./index.module.css";
 
-import MessageList from "../components/MessageList";
-import MessageInput from "../components/MessageInput";
-import Bar from "../components/Bar";
-import Agenda from "../components/Agenda";
-import Infobox from "../components/Infobox";
 import WelcomeForm from "../components/WelcomeForm";
 import { useEffect, useState } from "react";
-import { Info } from "../models/schema";
 import ActualIndex from "../components/ActualIndex";
 
 const socket: SocketIOClient.Socket = socketIOClient(
@@ -20,7 +14,6 @@ const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
 const Index = () => {
   const { data, error } = useSWR("/api/state", fetcher);
-  const [isUserRegistered, setIsUserRegistered] = useState(false);
   const [backgroundImage, setBackgroundImage] = useState("");
 
   useEffect(() => {
